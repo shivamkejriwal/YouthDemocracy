6d5ca07c06fd0313fd1101fbea25bf2f7ffd79c4
shivamkejriwal <Shivamkejriwal@gmail.com>
2013-02-03 21:08:17 -0600

    Initial Re-Commit

--- YouthDemocracy/war/js/forum.js
+++
@@ -1,9 +0,0 @@
-//Hides all apges and shows only desired page
-var goToForumPage = function(pageName)
-{
-	$('#Forum-category').hide();
-	$('#Forum-topic').hide();
-	$('#Forum-posts').hide();
-	
-	$('#Forum-' + pageName).show();
-}
